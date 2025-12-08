71bdcf295e299e38784f58f0153cfafea8c031f6
aziham <hamza.aziane@pm.me>
2025-12-08 22:28:33 +0100

    fix: Add PATCH to CORS allowed methods (#92)

--- backend/src/server/index.ts
+++ backend/src/server/index.ts
@@ -47,7 +47,7 @@ app.use((req: any, res: any, next: any) => {
     res.setHeader("Access-Control-Allow-Origin", "*");
     res.setHeader(
         "Access-Control-Allow-Methods",
-        "GET,POST,PUT,DELETE,OPTIONS",
+        "GET,POST,PUT,PATCH,DELETE,OPTIONS",
     );
     res.setHeader(
         "Access-Control-Allow-Headers",
